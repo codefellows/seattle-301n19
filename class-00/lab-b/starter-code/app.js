fb9dcaee5f668b61ed0e7f7538cf87242eca05e3
nebiyu kifle <rema9.ne@gmail.com>
2020-09-26 21:09:05 -0700

    do

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -199,7 +199,7 @@ Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//convert the resalt
 // 2. What is "this" when joe.scopeArrow() is invoked?
 //
 // 3. Explain why "this" is different when an arrow function is used.
