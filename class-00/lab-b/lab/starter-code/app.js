bf496a9d37933acf7b2107c160c87df8f5e5dd0c
Klace Koch <koch.klace@gmail.com>
2020-10-01 18:05:57 -0700

    going to commit after each step

--- class-00/lab-b/lab/starter-code/app.js
+++ class-00/lab-b/lab/starter-code/app.js
@@ -59,8 +59,8 @@ console.log('Let\'s do some math:', add(4, 5));
 const addOneLiner = (num1, num2) => `${num1} + ${num2} = ${num1 + num2}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Add as a one-liner:', addOneLiner(4, 5));
-
+console.log('Add as a one-liner:', addOneLiner(4, 5));
+// done
 
 // STEP 7
 // What if we have multiple lines of code?
